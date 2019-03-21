37e08a594a7e6e63d36b0e57b358a2c01e229563
rhmerx <huynh.r.k@gmail.com>
2019-03-20 20:38:24 -0400

    changes

--- tuplesAndSets/index.py
+++ tuplesAndSets/index.py
@@ -12,4 +12,10 @@ output = 6 in s
     # print(num)
 
 # can also create a set be using set('iterable item)
-print(set(['Los Angeles', 'Tokyo', 'Berlin', 'Osaka', 'Toronto', 'Vancouver', 'Vancouver', 'Tokyo']))
+cities = set(['Los Angeles', 'Tokyo', 'Berlin', 'Osaka', 'Toronto', 'Vancouver', 'Vancouver', 'Tokyo'])
+
+otherCities = {'Calgary', 'Houston', 'Berlin', 'Tokyo'}
+
+# set Math allows you to combine two sets and make a unique set using the pipe or using the ampersand to find the matching values between the two sets
+cities | otherCities # will generate a unique list
+cities & otherCities # will generate a set where the two sets have matches
